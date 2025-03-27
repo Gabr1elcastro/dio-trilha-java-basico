00d9f7669c71d594d6b84deadd208f1503f6b062
Gabr1elcastro <eg7112837@gmail.com>
2025-03-27 17:38:22 -0300

    Adcionando a classe Scanner, para facilitar  o uso do programa

--- src/utilizando_o_terminal/AboutMe.java
+++ src/utilizando_o_terminal/AboutMe.java
@@ -1,11 +1,24 @@
 package utilizando_o_terminal;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        //para passar os parametros via terminal, basta localizar o diretorio e digitar java nomeDoArquivo (sem o ".java") e os parametros separados por espaço. 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
