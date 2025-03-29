1b477a4e7420a8f576a23a6b6f2071a345a150d6
Gabr1elcastro <eg7112837@gmail.com>
2025-03-29 19:00:47 -0300

    começando condicionais

---
+++ src/controle_de_fluxo/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+package controle_de_fluxo;
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+    int nota = 8;
+
+    if(nota >= 7)
+    System.out.println("APROVADO");
+    else 
+        System.out.println("REPROVADO");
+    }
+}
