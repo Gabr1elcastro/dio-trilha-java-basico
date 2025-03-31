f93aaf9b840c372f35879dffaf3bbc9c12a9abe0
Gabr1elcastro <eg7112837@gmail.com>
2025-03-31 15:00:17 -0300

    Switch case

--- src/controle_de_fluxo/ResultadoEscolar.java
+++ src/controle_de_fluxo/ResultadoEscolar.java
@@ -1,8 +1,8 @@
 package controle_de_fluxo;
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
-        String resultado = nota >=7 ? "Aprovado!" : "Reprovado!"; //Expressão ternaria
+        int nota = 3;
+        String resultado = nota >=7 ? "Aprovado!" : nota >=5 && nota <7 ? "Recuperação!" : "Reprovado!";//Expressão ternaria
         System.out.println(resultado);
     }
 }
