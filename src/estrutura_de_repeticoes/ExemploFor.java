da913147c673c854b15771c77b1a0973e6f0e45b
Gabr1elcastro <eg7112837@gmail.com>
2025-04-08 20:05:56 -0300

    contagem decrescente

--- src/estrutura_de_repeticoes/ExemploFor.java
+++ src/estrutura_de_repeticoes/ExemploFor.java
@@ -2,11 +2,9 @@ package estrutura_de_repeticoes;
 
 public class ExemploFor {
     public static void main(String[] args) {
-        for (int n =1 ; n <=20; n ++ ){
-            if(n %2 ==0){
-                System.out.println(n);
-            }
-        }
-        System.out.println("como diria mnh ex, terminamos!");
+       for (int n= 10; n >= 1; n --){
+        System.out.println("contando" + n);
+       }
+       System.out.println("Terminamos a contagem!");
     }
 }
